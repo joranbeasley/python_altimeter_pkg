1e882529b422427c422d10418061bc54f50398b0
joran <joran@decagon.com>
2020-11-18 17:44:14 -0800

    added tmux conf script

--- install_scripts/wget_installer.sh
+++ install_scripts/wget_installer.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install git
+# sudo apt-get update
+sudo apt-get -y install git
 cd /home/pi
 git clone https://github.com/joranbeasley/python_altimeter_pkg.git
 cd python_altimeter_pkg/install_scripts
